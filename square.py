babe6915f80f55ffb364db3c87c200294a3d2471
Aarushi Tiwari <74093920+aarushi102@users.noreply.github.com>
2021-02-13 21:57:13 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,27 @@
+import turtle
+
+aaru_turtle = turtle.Turtle()
+
+
+def square():
+
+    aaru_turtle.forward(100)
+    aaru_turtle.right(90)
+    aaru_turtle.forward(100)
+    aaru_turtle.right(90)
+    aaru_turtle.forward(100)
+    aaru_turtle.right(90)
+    aaru_turtle.forward(100)
+
+
+square()
+aaru_turtle.forward(50)
+
+square()
+aaru_turtle.forward(50)
+
+square()
+aaru_turtle.forward(50)
+
+square()
+aaru_turtle.forward(50)
